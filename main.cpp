0ab6fdbd50e7d3244261dcefbc0d297af2f33169
Arthur Kung <arthurkung1006@gmail.com>
2020-10-22 23:40:50 +0100

    If clause

--- main.cpp
+++ main.cpp
@@ -4,7 +4,13 @@
 int main(int argc, char** argv) {
     int a;
     a=1;
-    std::cout << a;
+
+    std::cout << "IF clause\n";
+    if(a==0){
+        std::cout << 2;
+    } else {
+        std::cout << 3;
+    }
 
     return (EXIT_SUCCESS);
 }
