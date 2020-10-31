c3f9ab62955eb816075ad98105b13745737fa7ab
Arthur Kung <arthurkung1006@gmail.com>
2020-10-31 15:41:36 +0000

    add createCompanyEntry function to create a nested struct from struct inputs

--- structExample/structExample.cpp
+++ structExample/structExample.cpp
@@ -7,7 +7,6 @@ struct Employee
     double wage;
 };
 
-
 void printEmployeeInformation(Employee employee)
 {
     std::cout << "ID:   " << employee.id << '\n';
@@ -15,21 +14,42 @@ void printEmployeeInformation(Employee employee)
     std::cout << "Wage: " << employee.wage << '\n';
 }
 
+// Nested struct example
+struct Company
+{
+    Employee CEO; // Employee is a struct within the Company struct
+    int numberOfEmployees;
+};
+
+Company createCompanyEntry(Employee CEO, int numberOfEmployees)
+{
+    return { CEO, numberOfEmployees };// type company is already specified
+}
 int main()
 {
-    Employee joe{ 14, 32, 24.15 };
-    Employee frank{ 15, 28, 18.27 };
-
-    // Print Joe's information
-    printEmployeeInformation(joe);
-
-    // Nested struct example
-    struct Company
-    {
-        Employee CEO; // Employee is a struct within the Company struct
-        int numberOfEmployees;
+    Employee joe{ 
+        14,     //ID
+        32,     //age
+        24.15   //wage
+    };
+    Employee frank{ 
+        15,     //ID
+        28,     //age
+        18.27   //wage
     };
 
-    Company myCompany{ { 1, 42, 60000.0 }, 5 };
+    // Print Joe's information
+    std::cout << "Information of employee Joe:\n";
+    printEmployeeInformation(joe);
+
+    // Three methods of creating a company entry
+    Company myCompany1{ { 1, 42, 60000.0 }, 5 };
+    Company myCompany2{ joe, 12 };
+    Company myCompany3{ };
+    myCompany3 = createCompanyEntry(frank, 120);
+
+    //print the information of CEO of mycompany3
+    std::cout << "Information of CEO of mycompany3:\n";
+    printEmployeeInformation(myCompany3.CEO);
     return 0;
 }
