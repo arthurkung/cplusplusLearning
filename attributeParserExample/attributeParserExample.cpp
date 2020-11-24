ed2566fdc69dc36f0ed56642d7d2724f4133d210
Arthur Kung <arthurkung1006@gmail.com>
2020-11-24 00:53:01 +0000

    add getAttribute and getAttributeValue methods to TagInput

--- attributeParserExample/attributeParserExample.cpp
+++ attributeParserExample/attributeParserExample.cpp
@@ -12,29 +12,60 @@ using namespace std;
 class TagInput
 {
 public:
-    std::string m_string;
     bool isNewTag() {
         constexpr char tagClosureIdentifier = '/';
         return m_string[tagStartLoc] != tagClosureIdentifier;
     }
     std::string getTagName() {
-        constexpr char tagDelimiter = ' ';
-        return getStringUpToNext(tagDelimiter,tagStartLoc, false);
-    }
-    std::string getStringUpToNext(const char charToMatch, const int startingPos, const bool includeChar)
-    {
-        return m_string.substr(startingPos, m_string.find(charToMatch, startingPos) - (includeChar ? 0 : 1));
+        constexpr char tagDelimiter = '|';
+        return getStringUpToNext(tagDelimiter,tagStartLoc);
     }
     
-
+    std::string getNextAttributeValue() {
+        constexpr char quotes = '"';
+        getStringUpToNext(quotes);
+        std::string attributeValue{ getStringUpToNext(quotes) };
+        return trim(attributeValue);
+    }
+    std::string getNextAttribute() {
+        constexpr char eqSign = '=';
+        std::string attribute{ getStringUpToNext(eqSign) };
+        if (attribute == "Error: char not matched!")
+        {
+            return "No attributes found";
+        }
+        return trim(attribute);
+    }
+    void setupInput(std::istream& in) {
+        getline(in, m_string);
+    }
+    void printString() {
+        std::cout << m_string<<"\n";
+    }
 private:
     static constexpr int tagStartLoc = 1;
-    std::string& trim(std::string& str, char charToTrim)
+    std::string m_string;
+    std::string& trim(std::string& str, char charToTrim=' ')
     {
         std::string::size_type start{ str.find_first_not_of(charToTrim) };
         std::string::size_type end{ str.find_last_not_of(charToTrim) };
         str = str.substr(start, end - start + 1);
         return str;
+    }
+    std::string getFirstPartOfString(const std::string::size_type matchLoc, const int startingPos = 0)
+    {
+        std::string result{ m_string.substr(startingPos,  matchLoc - startingPos) };
+        m_string.erase(0, matchLoc+1);
+        return result;
+    }
+    std::string getStringUpToNext(const char charToMatch, const int startingPos=0)
+    {
+        std::string::size_type matchLoc{ m_string.find(charToMatch, startingPos) };
+        if (matchLoc == m_string.npos)
+        {
+            return "Error: char not matched!";
+        }
+        return getFirstPartOfString(matchLoc, startingPos);
     }
 };
 
@@ -64,6 +95,13 @@ public:
         m_name = inputStr;
     }
     void setAttributes(TagInput& tagInput) {
+        std::string attribute{};
+        std::string attributeValue{};
+        while ((attribute  = tagInput.getNextAttribute()) != "No attributes found") {
+            attributeValue = tagInput.getNextAttributeValue();
+            std::cout << "attribute: " << attribute << "; value: " << attributeValue << "\n";
+        }
+        
         return;
     }
 };
@@ -79,7 +117,7 @@ int main() {
 
     for (int i{}; i < tagLines; ++i)
     {
-        getline(cin, input.m_string);
+        input.setupInput(std::cin);
         if (input.isNewTag()) {
             currentTag = currentTag->addChildTag();
             currentTag->setTagName(input.getTagName());
@@ -92,7 +130,11 @@ int main() {
     }
     
     std::cout << adamTag.m_children[0].m_name;
-
+    //std::string a{ "this is a string" };
+    //std::string::size_type i{};
+    //std::cout << ((i = a.find(' ')) != a.npos);
+    //std::cout << a.find(' ', 6);
+    //std::cout << ((i = a.find('x')) != a.npos);
 
     return 0;
 }
